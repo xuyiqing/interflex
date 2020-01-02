84c7ae20e763ec456aa9df9d8eee6d5310daa104
Ziyi Liu <zyliu2016@nsd.pku.edu.cn>
2020-01-02 09:28:03 +0800

    v1.1.0

--- R/vcluster.R
+++ R/vcluster.R
@@ -12,12 +12,30 @@ vcovCluster <- function(
     M <- length(unique(cluster))
     N <- length(cluster)           
     K <- model$rank   
+	dfc <- (M/(M - 1)) * ((N - 1)/(N - K))
     ## if(M<50){
     ##     warning("Fewer than 50 clusters, variances may be unreliable (could try block bootstrap instead).")
     ## }
-    dfc <- (M/(M - 1)) * ((N - 1)/(N - K))
-    uj  <- apply(estfun(model), 2, function(x) tapply(x, cluster, sum));
-    rcse.cov <- dfc * sandwich(model, meat. = crossprod(uj)/N)
+    est.matrix <- estfun(model)
+	est.colname <- colnames(est.matrix)
+	est.ori.length <- length(est.colname)
+	coef.colname <- names(model$coefficients)
+	diff.name <- coef.colname[-which(coef.colname %in% est.colname)]
+	if(length(diff.name)>0){
+		for(char in diff.name){
+		est.matrix <- cbind(est.matrix,rep(0,N))
+		est.colname <- c(est.colname,char)
+	}
+		colnames(est.matrix) <- est.colname
+	}
+
+	uj  <- apply(est.matrix, 2, function(x) tapply(x, cluster, sum))
+	bread.empty <- matrix(0,nrow = length(coef.colname),ncol = length(coef.colname))
+	colnames(bread.empty) <- est.colname
+	rownames(bread.empty) <- est.colname
+	
+	bread.empty[1:est.ori.length,1:est.ori.length] <- bread(model)
+	rcse.cov <- dfc * sandwich(model,bread. = bread.empty, meat. = crossprod(uj)/N)
     ##colnames(rcse.cov)<-rownames(rcse.cov)<-names(model$coefficients)
     return(rcse.cov)
 }
