d9c9e4da44758252d978062866d83c34dfd99fb1
TianzhuQin <Maple_Optboy@outlook.com>
2024-05-13 15:44:46 +0100

    Continuous treatment methods for random forest

--- inst/python/dml_continuous_treatment.py
+++ inst/python/dml_continuous_treatment.py
@@ -6,8 +6,8 @@ import numpy as np
 from econml.dml import DML, LinearDML, SparseLinearDML, CausalForestDML
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
-from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, HistGradientBoostingClassifier, HistGradientBoostingRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 
 def marginal_effect_for_continuous_treatment(df, ml_method, Y, D, X, Z, d_ref = 1,
                           n_estimators=500, solver='adam', max_iter=10000, alpha=1e-5, hidden_layer_sizes=(5, 3, 2), random_state=1,
@@ -24,65 +24,77 @@ def marginal_effect_for_continuous_treatment(df, ml_method, Y, D, X, Z, d_ref = 
     random_state = int(random_state)
     poly_degree = int(poly_degree)
     casual_forest_n_estimators = int(casual_forest_n_estimators)
-    # get labels of covariates
     if type(Z) is str:
         Z = [Z]
     covariates = Z.copy()
     covariates.append(X)
 
-    df_base_index = df[D] == "Group.1"
-    df.loc[df_base_index, D] = int(0)
-    df.loc[~df_base_index, D] = int(1)
-    df[D] = df[D].astype('int')
-        
+    if len(df[Y].unique()) > 5:
+        discrete_outcome = False
+    else:
+        discrete_outcome = True
+
+    ## Below are methods for binary treatment 
+    # df_base_index = df[D] == "Group.1"
+    # df.loc[df_base_index, D] = int(0)
+    # df.loc[~df_base_index, D] = int(1)
+    # df[D] = df[D].astype('int')
+
+    ### Choose ML method 
     ml_method_lower = ml_method.lower()
-    ### First stage: estimate nuisance parameters
-    # create random forest estimators for nuisance parameters g and m
     if ml_method_lower in {"randomforest", "random forest", "random_forest", "rf", "forest"}:
-        ml_reg = RandomForestRegressor(n_estimators=n_estimators)
+        model_y = RandomForestClassifier(n_estimators=n_estimators) if discrete_outcome else RandomForestRegressor(n_estimators=n_estimators)
+        model_t = RandomForestRegressor(n_estimators=n_estimators)
 
     elif ml_method_lower in {"boosting", "gradient_boosting", "gradient boosting",
                           "hist_gradient_boosting", "hist gradient boosting",
                           "boost", "gradient_boost", "gradient boost", 
                           "hist_gradient_boost", "hist gradient boost",
                           "b", "gb", "hgb"}:                  
-        ml_reg = HistGradientBoostingRegressor()
+        model_y = HistGradientBoostingClassifier() if discrete_outcome else HistGradientBoostingRegressor()
+        model_t = HistGradientBoostingRegressor()
 
     elif ml_method_lower in {"network", "neural_network", "neural network", "nn"}:
-        ml_reg = MLPRegressor(solver=solver, max_iter=max_iter, alpha=alpha, hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
+        model_y = MLPClassifier(solver=solver, max_iter=max_iter, alpha=alpha, hidden_layer_sizes=hidden_layer_sizes, random_state=random_state) if discrete_outcome else MLPRegressor(solver=solver, max_iter=max_iter, alpha=alpha, hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
+        model_t = MLPRegressor(solver=solver, max_iter=max_iter, alpha=alpha, hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
 
     else:
         print("'ml_method' should be one of 'rf', 'hgb', and 'nn'.")
         return None
 
-    length = 50
-
-    new_data = {"x": np.linspace(df[X].min(), df[X].max(), length)}
-
+    ### Choose DML method
     dml_method_lower = dml_method.lower()
     if dml_method_lower == "default"[:len(dml_method_lower)]:
-        est = LinearDML(model_y=ml_reg, model_t=ml_reg, random_state=random_state)
+        est = LinearDML(model_y=model_y, model_t=model_t, random_state=random_state, discrete_outcome=discrete_outcome)
+
     elif dml_method_lower == "polynomial"[:len(dml_method_lower)]:
-        est = SparseLinearDML(model_y=ml_reg, model_t=ml_reg,
+        est = SparseLinearDML(model_y=model_y, model_t=model_t,
                             featurizer=PolynomialFeatures(degree=poly_degree),
-                            random_state=random_state)
+                            random_state=random_state, discrete_outcome=discrete_outcome)
+        
     elif dml_method_lower == "regularization"[:len(dml_method_lower)]:
-        est = DML(model_y=ml_reg, model_t=ml_reg,
+        est = DML(model_y=model_y, model_t=model_t,
                   model_final=Lasso(alpha=lasso_alpha, fit_intercept=False),
                   featurizer=PolynomialFeatures(degree=poly_degree),
-                  random_state=random_state)
+                  random_state=random_state, discrete_outcome=discrete_outcome)
+        
     elif dml_method_lower == "non-parametric"[:len(dml_method_lower)]:
-        est = CausalForestDML(model_y=ml_reg, model_t=ml_reg,
+        est = CausalForestDML(model_y=model_y, model_t=model_t,
                               criterion=casual_forest_criterion, 
                               n_estimators=casual_forest_n_estimators,
                               min_impurity_decrease=casual_forest_in_impurity_decrease,
-                              random_state=random_state)
+                              random_state=random_state, discrete_outcome=discrete_outcome)
         est.tune(df[[Y]], df[[D]], X=df[[X]], W=df[[*Z]])
+
     else:
         print("'dml_method' should be one of 'default', 'polynomial', 'regularization', and 'non-parametric'.")
         return None
 
     est.fit(df[[Y]], df[[D]], X=df[[X]], W=df[[*Z]])
+    
+    ### Get results
+    length = 50
+    new_data = {"x": np.linspace(df[X].min(), df[X].max(), length)}
     df_cate = est.effect_inference(new_data["x"].reshape(length, 1), T0=0, T1=d_ref).summary_frame(alpha=0.05, value=0, decimals=99)
     df_cate["X"] = new_data["x"]
     df_cate["ME"] = df_cate["point_estimate"]
