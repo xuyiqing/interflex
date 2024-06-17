361def39e754c2202ef5bfd37991976e2ed94926
Yiqing Xu <xuyiqing@users.noreply.github.com>
2024-06-17 14:47:52 -0700

    v1.3.0 beta

--- R/zzz.r
+++ R/zzz.r
@@ -2,5 +2,5 @@
 function (libname, pkgname){
    # echo output to screen
    packageStartupMessage("## Syntax has changed since v.1.2.1.\n")
-   packageStartupMessage("## See http://bit.ly/interflex for more info.\n## Comments and suggestions -> zyliu2020@uchicago.edu.\n")
+   packageStartupMessage("## See https://yiqingxu.org/packages/interflex/ for more info.\n## Comments and suggestions -> yiqingxu@stanford.edu.\n")
 }
