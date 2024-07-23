aeef1bce26cbf7b22b7307b9a6c08a35a1d451b9
Ziyi Liu <zyliu2016@nsd.pku.edu.cn>
2024-07-23 20:43:49 +0800

    Add files via upload

--- R/plot.R
+++ R/plot.R
@@ -563,7 +563,7 @@ plot.interflex <- function(x,
             for (char in other.treat) {
                 if (CI == TRUE) {
                     yrange <- c(yrange, na.omit(unlist(c(est.lin[[char]][, c(4, 5)]))))
-                    if(ncol(est.kernel[[char]])>5){
+                    if(ncol(est.lin[[char]])>5){
                         yrange <- c(yrange, na.omit(unlist(c(est.lin[[char]][, c(6, 7)]))))
                     }
                 } else {
@@ -579,7 +579,7 @@ plot.interflex <- function(x,
             for (label in label.name) {
                 if (CI == TRUE) {
                     yrange <- c(yrange, na.omit(unlist(c(est.lin[[label]][, c(4, 5)]))))
-                    if(ncol(est.kernel[[char]])>5){
+                    if(ncol(est.lin[[char]])>5){
                         yrange <- c(yrange, na.omit(unlist(c(est.lin[[char]][, c(6, 7)]))))
                     }
                 } else {
